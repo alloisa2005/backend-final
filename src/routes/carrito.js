a45579f32150d1c4a04ca7a1b5f696f402016a15
Anthony Allois <anthonyallois@gmail.com>
2022-09-21 11:00:43 -0300

    Creando rutas Carrito

--- src/routes/carrito.js
+++ src/routes/carrito.js
@@ -3,8 +3,30 @@ import { Router } from 'express';
 
 const router = Router();
 
+let carritos = [];
+
+// Middleware para validar si existe el ID del carrito
+const validarCarritoId = (req,res,next) => {
+  let { id } = req.params;  
+
+  let existe = carritos.some(p => p.id === parseInt(id)); 
+
+  existe ? next() : res.status(200).send({ status:'ERROR', result: `No existe carrito con ID ${id}`});
+}
+
 router.get('/', (req, res) => {
   res.send({status:'OK', result:'Hola Carrito'});
+});
+
+router.get('/:id', validarCarritoId, (req, res) => {
+
+  try {
+    let { id } = req.params;    
+    let carro = carritos[id-1];    
+    res.status(200).send({status:'OK', result: carro}); 
+  } catch (error) {
+    res.status(404).send({status:'ERROR', result: error.message}); 
+  }  
 });
 
 export default router;
