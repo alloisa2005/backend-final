d4b912f4d6e42360e3845ae2b98f8281f85409cb
Anthony Allois <anthonyallois@gmail.com>
2022-09-23 21:35:50 -0300

    Agregando rutas de Carrito

--- src/models/Cart.js
+++ src/models/Cart.js
@@ -7,43 +7,6 @@ export default class Cart {
     this.archivo = archivo;    
   }
   
-  getAll = async () => {
-    try {
-      if(fs.existsSync(this.archivo)){
-
-        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
-        let data = JSON.parse(arch);
-              
-        return {status: 'OK', result: data};
-
-      }else {
-        return {status: 'OK', result: []}
-      }      
-
-    } catch (error) {
-      return {status: 'ERROR', result: error.message}
-    }
-  }
-
-  getById = async (id) => { 
-    try {
-      if(fs.existsSync(this.archivo)){
-        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
-        let data = JSON.parse(arch);
-        let obj = data.find( (item) => item.id === parseInt(id));
-
-        if(obj) return {status: 'OK', result: obj};
-
-        return {status:'ERROR', result:`No existe el producto ID ${id}`}
-        
-      }else {
-        return {status:'ERROR', result:`No existe el producto ID ${id}`}
-      }      
-    } catch (error) {
-      return {status:'ERROR', result: error.message}
-    }
-  }
-
   addCart = async (obj) => {
     try {
       if(fs.existsSync(this.archivo)){
@@ -60,6 +23,64 @@ export default class Cart {
         await fs.promises.writeFile(this.archivo, JSON.stringify([obj], null, 2))
         return {status: "OK", result: obj.id}
       }
+    } catch (error) {
+      return {status: 'ERROR', result: error.message}
+    }
+  }
+
+  getById = async (id) => { 
+    try {
+      if(fs.existsSync(this.archivo)){
+        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
+        let data = JSON.parse(arch);
+        let obj = data.find(c => c.id === parseInt(id));
+
+        if(!obj) return {status: 'ERROR', result: `Carrito ID: ${id} no existe`}
+
+        let productos = obj.productos;        
+        return {status: 'OK', result: productos}
+
+      }else {
+        return {status:'ERROR', result: `Carrito ID: ${id} no existe`}
+      }      
+    } catch (error) {
+      return {status:'ERROR', result: error.message}
+    }
+  }
+
+  addProductsToCart = async (id, products) => {
+    try {
+      if(fs.existsSync(this.archivo)){
+        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
+        let data = JSON.parse(arch);
+        let obj = data.find(c => c.id === parseInt(id));
+        
+        products.productos.forEach(p => obj.productos.push(p));
+        console.log(obj.productos);
+
+        await fs.promises.writeFile(this.archivo,JSON.stringify(data,null,2))     
+        return {status:'OK', result: `Productos agregados al carrito ID: ${id}`}   
+      } else {
+        return {status:'ERROR', result: `Carrito ID: ${id} no existe`}
+      }
+    } catch (error) {
+      return {status:'ERROR', result: error.message}
+    }
+  }
+
+  getAll = async () => {
+    try {
+      if(fs.existsSync(this.archivo)){
+
+        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
+        let data = JSON.parse(arch);
+              
+        return {status: 'OK', result: data};
+
+      }else {
+        return {status: 'OK', result: []}
+      }      
+
     } catch (error) {
       return {status: 'ERROR', result: error.message}
     }
--- src/routes/carrito.js
+++ src/routes/carrito.js
@@ -28,14 +28,25 @@ router.post('/', async (req, res) => {
 //TODO: Vacía un carrito y lo elimina
 router.delete('/:id', (req, res) => {});
 
-//TODO: permite listar todos los productos guardados en el carrito
-router.get('/:id/productos', (req, res) => {});
+router.get('/:id/productos', async (req, res) => {
 
-//TODO: incorporar productos al carrito por su id de carrito
-router.post('/:id/productos', (req, res) => {});
+  let { id } = req.params;
+  let respuesta = await cartContainer.getById(id);
+
+  res.send({status: 'OK', result: respuesta.result});
+});
+
+router.post('/:id/productos', async (req, res) => {
+
+  let { id } = req.params;
+  let productos = req.body;  
+  let respuesta = await cartContainer.addProductsToCart(id, productos);
+
+  res.send({status: respuesta.status, result: respuesta.result});
+});
 
 //TODO: Eliminar un producto del carrito por su id de carrito y de producto  
-router.delete('/:id/productos/:id_prod', (req, res) => {});
+router.delete('/:id/productos/:id_prod', (req, res) => {}); 
 
 
 export default router;
